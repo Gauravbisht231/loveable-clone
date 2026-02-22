3aa5a10f790a60b75025586b1109160f1cf9b128
Gauravbisht231 <gaurav.bisht.ug20@nsut.ac.in>
2026-02-22 14:48:22 +0530

    initial commit

---
+++ src/test/java/com/gaurav/projects/loveable_clone/LoveableCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaurav.projects.loveable_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveableCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
